dcd21ceee5e77f811d1ac6b9e2a4a2aa60ac6e0a
Pavel Rusakov <malius@tutamail.com>
2025-04-21 17:41:05 +0800

    first real clients for log, cfg and db

--- main.go
+++ main.go
@@ -1,152 +1,66 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/rskv-p/mini/srv"
-	"gorm.io/gorm"
+	"github.com/rskv-p/mini/pkg/x_bus"
+	"github.com/rskv-p/mini/pkg/x_cfg"
+	"github.com/rskv-p/mini/pkg/x_db"
+	"github.com/rskv-p/mini/pkg/x_log"
+	"github.com/rskv-p/mini/pkg/x_req"
 )
 
 func main() {
-	// Create mock client instances
-	dbClient := &MockDBClient{}
-	logClient := &MockLogClient{}
-	busClient := &MockBusClient{}
-	cfgClient := &MockConfigClient{}
+	// Initialize the bus
+	bus := x_bus.NewBus(false, "11111")
 
-	// Create the service
-	service := srv.New("TestService", dbClient, logClient, busClient, cfgClient)
+	// Create the clients
+	dbClient := x_db.NewClient(1, bus)
+	logClient := x_log.NewClient(2, bus)
+	cfgClient := x_cfg.NewClient(3, bus)
 
-	// Start the service
-	if err := service.Start(); err != nil {
-		log.Fatalf("Failed to start service: %v", err)
+	// Set up message handlers for each client to process messages
+	// Subscribing to the correct subjects only
+	err := dbClient.Subscribe("db.create") // DB client only subscribes to db.create
+	if err != nil {
+		log.Printf("Error subscribing DB client: %v", err)
 	}
 
-	// Stop the service (this would normally happen when shutting down)
-	if err := service.Stop(); err != nil {
-		log.Fatalf("Failed to stop service: %v", err)
+	err = logClient.Subscribe("log.message") // Log client only subscribes to log.message
+	if err != nil {
+		log.Printf("Error subscribing Log client: %v", err)
 	}
-}
 
-// Mock DBClient
-type MockDBClient struct{}
+	err = cfgClient.Subscribe("config.update") // Config client only subscribes to config.update
+	if err != nil {
+		log.Printf("Error subscribing Config client: %v", err)
+	}
 
-func (m *MockDBClient) GetDB() (*gorm.DB, error) {
-	fmt.Println("Mock DB Client: GetDB called")
-	return nil, nil
-}
+	// Define custom message handlers
+	cfgClient.HandleMessage = func(req *x_req.Request) {
+		log.Printf("Custom handler for Config Client - Subject: %s, Data: %s", req.Subject, string(req.Data))
+	}
 
-func (m *MockDBClient) Migrate(models ...interface{}) error {
-	fmt.Println("Mock DB Client: Migrate called")
-	return nil
-}
+	// Publish messages to simulate actions
+	log.Println("Publishing messages...")
 
-func (m *MockDBClient) First(dest interface{}, conds ...interface{}) *gorm.DB {
-	fmt.Println("Mock DB Client: First called")
-	return nil
-}
+	// Simulate DB create operation
+	err = dbClient.Create("some_model", "model1")
+	if err != nil {
+		log.Printf("Error in DB create operation: %v", err)
+	}
 
-func (m *MockDBClient) Create(model interface{}) error {
-	fmt.Println("Mock DB Client: Create called")
-	return nil
-}
+	// Simulate Log message publishing
+	logClient.Log("INFO", "Service started")
 
-func (m *MockDBClient) Find(model interface{}, conditions ...interface{}) error {
-	fmt.Println("Mock DB Client: Find called")
-	return nil
-}
+	// Simulate Config update operation
+	err = cfgClient.SetConfig("log_level", "DEBUG")
+	if err != nil {
+		log.Printf("Error in Config update operation: %v", err)
+	}
 
-// Mock LogClient
-type MockLogClient struct{}
-
-func (m *MockLogClient) Trace(message string, context map[string]interface{}) {
-	fmt.Println("Mock Log Client: Trace -", message)
-}
-
-func (m *MockLogClient) Debug(message string, context map[string]interface{}) {
-	fmt.Println("Mock Log Client: Debug -", message)
-}
-
-func (m *MockLogClient) Info(message string, context map[string]interface{}) {
-	fmt.Println("Mock Log Client: Info -", message)
-}
-
-func (m *MockLogClient) Warn(message string, context map[string]interface{}) {
-	fmt.Println("Mock Log Client: Warn -", message)
-}
-
-func (m *MockLogClient) Error(message string, context map[string]interface{}) {
-	fmt.Println("Mock Log Client: Error -", message)
-}
-
-// Mock BusClient
-type MockBusClient struct{}
-
-func (m *MockBusClient) Subscribe(subject string) error {
-	fmt.Println("Mock Bus Client: Subscribe called for subject:", subject)
-	return nil
-}
-
-func (m *MockBusClient) Unsubscribe(subject string) {
-	fmt.Println("Mock Bus Client: Unsubscribe called for subject:", subject)
-}
-
-func (m *MockBusClient) Deliver(subject string, data []byte) {
-	fmt.Println("Mock Bus Client: Deliver called for subject:", subject, "data:", string(data))
-}
-
-func (m *MockBusClient) Publish(subject string, data []byte) error {
-	fmt.Println("Mock Bus Client: Publish called for subject:", subject, "data:", string(data))
-	return nil
-}
-
-func (m *MockBusClient) PublishWithReply(subject string, data []byte, reply string) {
-	fmt.Println("Mock Bus Client: PublishWithReply called for subject:", subject, "data:", string(data), "reply:", reply)
-}
-
-func (m *MockBusClient) SubscribeWithQueue(subject string, queue string, handler func(subject string, msg []byte)) error {
-	fmt.Println("Mock Bus Client: SubscribeWithQueue called for subject:", subject, "queue:", queue)
-	return nil
-}
-
-func (m *MockBusClient) GetClientCount() int {
-	return 1
-}
-
-func (m *MockBusClient) GetMsgHandlerCount() int {
-	return 1
-}
-
-// Mock ConfigClient
-type MockConfigClient struct{}
-
-func (m *MockConfigClient) GetConfig(key string) any {
-	fmt.Println("Mock Config Client: GetConfig called for key:", key)
-	return nil
-}
-
-func (m *MockConfigClient) SetConfig(key string, value any) error {
-	fmt.Println("Mock Config Client: SetConfig called for key:", key, "value:", value)
-	return nil
-}
-
-func (m *MockConfigClient) DeleteConfig(key string) error {
-	fmt.Println("Mock Config Client: DeleteConfig called for key:", key)
-	return nil
-}
-
-func (m *MockConfigClient) PublishConfig(key string, value any) error {
-	fmt.Println("Mock Config Client: PublishConfig called for key:", key, "value:", value)
-	return nil
-}
-
-func (m *MockConfigClient) LoadConfig(file string) error {
-	fmt.Println("Mock Config Client: LoadConfig called for file:", file)
-	return nil
-}
-
-func (m *MockConfigClient) ReloadConfig() error {
-	fmt.Println("Mock Config Client: ReloadConfig called")
-	return nil
+	// Call HandleSubscribe to simulate the processing of messages
+	dbClient.HandleSubscribe()
+	logClient.HandleSubscribe()
+	cfgClient.HandleSubscribe()
 }
